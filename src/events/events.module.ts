cc3d021caac8a935abdd2df92d16ccf19ddaa5f5
JC-Coder <josephchimezie2003@gmail.com>
2022-10-28 22:43:07 +0100

    one to one relationship

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from '../auth/profile.entity';
+import { User } from '../auth/user.entity';
 import { Attendee } from './attendee.entity';
 import { Event } from './event.entity';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Event, Attendee])],
+    imports: [TypeOrmModule.forFeature([Event, Attendee, User, Profile])],
     controllers: [EventsController],
     providers: [EventsService]
 })
